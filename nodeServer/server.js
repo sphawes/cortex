65c63fccc03d8469166c6ba73038fb4cb4c7dfa6
sphawes <sphawes@gmail.com>
2017-02-17 11:11:31 -0500

    server.js should point to index.html now

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.listen(8081, function () {
