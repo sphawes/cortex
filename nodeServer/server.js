e3ef85a35703bb619fcb8a60b4d1ac1cc5d4e587
sphawes <sphawes@gmail.com>
2017-02-17 11:39:52 -0500

    server crapped its pants at port 80, weird, id like it to not require a :port at the end of the url.

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(80, function () {
-  console.log('Cortex server running on port 80!');
+app.listen(8081, function () {
+  console.log('Cortex server running on port 8081!');
 })
