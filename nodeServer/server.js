31fbeb8cd726d34cfbaf372936703161ef3635b1
sphawes <sphawes@gmail.com>
2017-02-17 11:16:43 -0500

    this time, included the path variable

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,10 +1,12 @@
-var express = require('express')
-var app = express()
+var path = require('path');
+
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + 'index.html'));
 })
 
 app.listen(8081, function () {
-  console.log('Example app listening on port 8081!')
+  console.log('Cortex server running on port 8081!');
 })
