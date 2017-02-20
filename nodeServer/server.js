3cab02b6e14d4a26eaf78d3f30395472f1495744
sphawes <sphawes@gmail.com>
2017-02-20 15:11:21 -0500

    trying to set up my own javascript file as a static file

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,30 +1,15 @@
 var path = require('path');
-
 var express = require('express');
 var app = express();
 
-//var main = require('main');
-
+app.use('/js', express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.get("/action", function(req, res){
-	console.log(req.query)
-	try{
-	    state = req.query.state
-	    if (state == "doorToggle") {
-      		result = execSync("python action.py doorToggle")
-      		console.log(result.toString())
-	    }
-	}
-  catch(e){
-	    res.status(500).send(e)
-	}
+app.get('')
 
-});
-
-app.listen(8081, function () {
+app.listen(8081, function() {
   console.log('Cortex server running on port 8081!');
 })
