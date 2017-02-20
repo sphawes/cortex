aab4b049927be02d7809e1491841e4eff294ad18
sphawes <sphawes@gmail.com>
2017-02-20 15:31:25 -0500

    alright. so. i cant get an external javascript file to run, so im just writing it inline, yo. it works, so here we are. now im going to dive into paths and try to figure out how to get python scripts to execute

--- nodeServer/js/main.js
+++ nodeServer/js/main.js
@@ -1,8 +1,1 @@
-
-
-
-$(document).ready(function(){
-    $("#frontDoorButton").click(function(){
-        $.get("../scripts/action.py", {"action": "doorToggle"})
-    });
-})
+alert("test");
--- nodeServer/server.js
+++ nodeServer/server.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use('/js', express.static('public'));
+app.use(express.static('js'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
