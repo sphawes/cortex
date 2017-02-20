38471aebc201b6d266f2c33cac24ff5cdbf881c1
sphawes <sphawes@gmail.com>
2017-02-20 10:08:07 -0500

    hackpgh scrape is working, but for some reason it doesnt load the actual image that shows up on the webpage. it's set up to be called as a funciton and return true for open false for closed or unexpected input

--- scripts/scrapeHackPGH.py
+++ scripts/scrapeHackPGH.py
@@ -3,14 +3,21 @@ import requests, bs4, os, re
 import urllib.request
 import shutil
 
+def isHackPghOpen():
 
-url = "http://www.hackpittsburgh.org/"
+    url = "http://www.hackpittsburgh.org/"
 
 
-#sweep categories page, bring up each category link
-res = requests.get(url)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
-#page is now soup-ified in BS4 element 'soup'
+    #sweep categories page, bring up each category link
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    #page is now soup-ified in BS4 element 'soup'
+    lightStatusPanel = soup.find("div", {"id": "light_status"});
 
-soup.find("div", {"id": "page_status"})
+    if lightStatusPanel['style'] == "width: 209px; height: 76px; margin-bottom: 0px; background: url('http://www.hackpittsburgh.org/wp-content/uploads/2016/12/shopClosed2.png'); display:none;":
+        return False;
+    elif lightStatusPanel['style'] == "width: 209px; height: 76px; margin-bottom: 0px; background: url('http://www.hackpittsburgh.org/wp-content/uploads/2016/12/shopClosed2.png'); display:none;":
+        
+    else:
+        return True;
