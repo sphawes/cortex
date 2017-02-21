690de829795a3c757147d1d78108c110c4c33ad1
sphawes <sphawes@gmail.com>
2017-02-21 15:07:02 +0000

    VE was back to 2.7 for some reason. reset it and pushing up now

--- cortexVE/bin/activate.fish
+++ cortexVE/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/stephen/Drive/CREATIVEPROJECTS/cortex/cortexVE"
+set -gx VIRTUAL_ENV "/home/sphawes/cortex/cortexVE"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
