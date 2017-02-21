20be7480eaaa04c510b332af987669975eef745d
sphawes <sphawes@gmail.com>
2017-02-21 15:49:46 -0500

    added js that alerts the status of hackpgh's door

--- nodeServer/public/main.js
+++ nodeServer/public/main.js
@@ -2,6 +2,11 @@ function doOnWindowLoad(){
   var d = new Date();
   document.getElementById("date").innerHTML = d.toDateString();
 
+  $.getJSON('http://iobridge.com/api/feed/key=waStTNuoEvld6t9wsM&callback=\?', function(response){
+       var data = response;
+       alert(data['modules']['channels'][0]['AnalogInput']);
+  })
+
 }
 window.onload = doOnWindowLoad;
 
